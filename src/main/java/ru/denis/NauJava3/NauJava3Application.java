5e63b5963c9276cf0a9cb88d0a39929867330735
Dbatr <bdanis2311@gmail.com>
2025-03-16 17:40:34 +0500

    Initial commit: empty Spring Boot project

---
+++ src/main/java/ru/denis/NauJava3/NauJava3Application.java
@@ -0,0 +1,13 @@
+package ru.denis.NauJava3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJava3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauJava3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/denis/NauJava3/NauJava3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denis.NauJava3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NauJava3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
